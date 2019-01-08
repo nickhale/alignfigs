bcdbc15a0b56c2e5b83b9b82ac5021487c45b8f5
Nick Hale <nickphale@gmail.com>
2019-01-08 17:23:45 +0200

    Major overhaul. Works most of the time.. (MATLAB graphics are not deterministic.)

--- alignfigs.m
+++ alignfigs.m
@@ -1,9 +1,9 @@
-function alignfigs(varargin)
+function alignfigs(nx)
 %alignfigs   Arrange all open figure windows nicely.
-%   ALIGNFIGS() arranges all the open current figure windows to fit nicely on the
-%   screen in a grid. If they won't fit, then the size is reduced uniformly.
+%   ALIGNFIGS() arranges all the open current figure windows to fit nicely on
+%   the screen in a grid as large as possible.
 %
-%   ALIGNFIGS(N) specifies that there should be N columns.
+%   ALIGNFIGS(NX) specifies that the grid should have NX columns.
 %
 %   Note that if not all windows have the same size, they will be adjusted to be
 %   so (to the smallest open window).
@@ -12,20 +12,41 @@ function alignfigs(varargin)
 %       close all, for k = 1:8, figure, end, alignfigs()
 %       close all, for k = 1:15, figure, end, alignfigs(5)
 
-% Nick Hale - Dec 2014.
+% Nick Hale - v1.0 Dec 2014.
+%           - v2.0 Jan 2019.
+
+padding = true;
+toolbars = false;
+menubars = false;
 
 h = findobj('Type', 'figure');
 nh = numel(h);
 
-% Windows are taller than they claim to be?
-fudge1 = 190; % Also the top on is different...
-fudgeN = 170;
+% Strip toolbars and menubars if requested:
+if ( ~toolbars )
+    set(h, 'toolbar', 'none')
+end
+if ( ~menubars )
+    set(h, 'Menubar', 'none')
+end
 
-% Use these values if removing the toolbar.
-% fudge1 = 130;
-% fudgeN = 100;
+% Get the screen size:
+ss = get(0, 'ScreenSize');
+ss = ss(3:4);
+sd = get(0, 'ScreenDepth');
+ss(1) = ss(1) - sd;
+ss(2) = ss(2) - 2*sd;
 
-if ( nh < 2 )
+if ( nh == 0 )
+    % Nothing to do!
+    return
+elseif ( nh == 1 )
+    p = zeros(1,4);
+    p(1,3) = ss(1);
+    p(1,4) = ss(2)-sd;
+    p(1:2) = [sd/2 3*sd/2];
+    set(h, 'OuterPosition', p);
+    figure(1)
     return
 end
 
@@ -34,77 +55,73 @@ end
 h = h(idx);
 
 % Get the sizes:
-for k = 1:numel(h)
-    p(k,:) = get(h(k), 'position');
-%     set(h(k), 'toolbar', 'none')
+r = {};
+for k = 1:nh
+    r{k} = get(h(k), 'Resize');
+    set(h(k), 'Resize', 'off')
+    p(k,:) = get(h(k), 'OuterPosition');
 end
-% Adjust to be the same size:
-p(:,3:4) = repmat(min(p(:,3:4)),nh, 1);
-
-% Get the screen size:
-ss = get(0, 'ScreenSize');
-ss = ss(3:4);
 
 % Determine the grid size:
-nxMax = floor(ss(1)/p(1,3));
 if ( nargin == 0 )
-    nx = nxMax;
+    nx = ceil(sqrt(nh));
+end
+ny = ceil(numel(h)/nx);
+if ( padding )
+    p(:,3) = .95*ss(1)/nx;
+    p(:,4) = .95*ss(2)/ny;
 else
-    nx = varargin{1};
+    p(:,3) = ss(1)/nx;
+    p(:,4) = ss(2)/ny;
+end
+p = round(p);
+
+% Horizontal padding:
+if ( padding )
+    padx = round(ss(1)/(nx+1) - nx*p(1,3)/(nx+1));
+    pady = round(ss(2)/(ny+1) - ny*p(1,4)/(ny+1));
+else
+    padx = 0;
+    pady = 0;
 end
 
-% Screen isn't large enough! Shrink the figures:
-ny = floor( (ss(2)+fudgeN) / (p(1,4)+fudgeN) );
-if ( nh > nx*ny || nx > nxMax )
-    for k = 1:nh
-        set(h(k), 'position', [p(k,1:2), .9*p(k,3:4)]);
-    end
-    alignfigs(varargin{:});
-    return
-end
-
-% Add a margin at the left to centre:
-leftMargin = (ss(1) - nx*p(1,3))/2;
-
-% Place the first figure in the top lefT:
-p(1,:) = [leftMargin ss(2) - p(1,4) p(1,3:4)];
-set(h(1), 'position', p(1,:));
-p(1,:) = get(h(1),'position');
+% Place the first figure in the top left:
+px = sd/2 + padx;
+py = ss(2) - p(1,4) + sd;
+p(1,1:2) = [px, py];
+set(h(1), 'OuterPosition', p(1,:));
 
 % Initialise px and py:
 if ( nx > 1 )
-    px = p(1,1) + p(1,3);
+    px = p(1,1) + p(1,3) + padx;
     py = p(1,2);
 else
     % Need to treat single column as a special case.
-    px = leftMargin;
-    py = p(1,2) - p(1,4) - fudge1;
+    px = sd/2 + padx;
+    py = p(1,2) - p(1,4) - pady;
 end
 
 % Loop over the remaining windows:
 for k = 2:numel(h)
     % Set to the new px and py values
     p(k,1:2) = [px, py];
-    set(h(k), 'position', p(k,:));
+    set(h(k), 'OuterPosition', p(k,:));
+   
     if ( mod(k, nx) )
         % Move to the next column:
-        px = px + p(k-1,3);
+        px = px + p(k-1,3) + padx;
     else
         % Move to the next row. 
-        px = leftMargin;
+        px = sd/2 +  padx;
         % Update py:
-        if ( k == nx )
-            % First row is a special case (not sure why!)
-            py = py - p(k-1,4) - fudge1;
-        else
-            py = py - p(k-1,4) - .5*fudgeN;
-        end
+        py = py - p(k-1,4) - pady;
     end        
 end
 
 % Bring all the windows to the front:
 for k = 1:nh
     figure(h(k))
+    set(h(k), 'Resize', r{k});
 end
 
 end
